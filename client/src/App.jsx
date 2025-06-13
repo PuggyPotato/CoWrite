91b7037a6b4d5b1bfd449f264aa9b430f0d9d06c
PuggyPotato <qingyaurou@gmail.com>
2025-06-13 23:40:49 +0800

    Added a drop down menu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,30 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [drop,setDrop] = useState(false)
 
+  function toggleDrop(){
+    if(drop == true){
+      setDrop(false);
+      console.log("toggle false")
+    }
+    else{
+      setDrop(true)
+      console.log("toggle true")
+    }
 
+  }
   return (
     <>
     <div id='container'>
       <div id="tools">
-        <button id='dropDown'>
-          <img src="./public/dropdown.png" alt='dropdown' id='dropDownImage'/>
+        <button id='dropDown' onClick={() =>toggleDrop()}>
+          <img src="./dropdown.png" alt='dropdown' id='dropDownImage'/>
         </button>
+      </div>
+      <div id='droppedDown' style={drop ? {display:"block"} : {display:"none"}}>
+            <button className='dropButton'>Save</button><br></br>
+            <button className='dropButton'>Exit</button>
       </div>
       <div id='contentContainer'>
         <textarea id='content'>
